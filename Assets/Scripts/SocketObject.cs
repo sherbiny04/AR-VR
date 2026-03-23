c4068df2981f207566ba17376cacf34a33a54ecd
sherbiny04 <Abdelrahman04.sherbiny@gmail.com>
2026-03-23 20:11:51 +0200

    update assets

---
+++ Assets/Scripts/SocketObject.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SocketObject : MonoBehaviour
+{
+
+    public UnityEngine.XR.Interaction.Toolkit.Interactors.XRSocketInteractor socket1;
+    public UnityEngine.XR.Interaction.Toolkit.Interactors.XRSocketInteractor socket2;
+    public UnityEngine.XR.Interaction.Toolkit.Interactors.XRSocketInteractor socket3;
+
+    public Animator coffinAnimator;
+    public AudioSource coffinAudio;
+
+    bool isCoffinOpen = false;
+    
+    void Start()
+    {
+    }
+
+    
+    void Update()
+    {
+        UnityEngine.XR.Interaction.Toolkit.Interactables.IXRSelectInteractable object1 = socket1.GetOldestInteractableSelected();
+        UnityEngine.XR.Interaction.Toolkit.Interactables.IXRSelectInteractable object2 = socket2.GetOldestInteractableSelected();
+        UnityEngine.XR.Interaction.Toolkit.Interactables.IXRSelectInteractable object3 = socket3.GetOldestInteractableSelected();
+
+        if (object1 != null && object2 != null && object3 != null)
+        {
+
+            if (object1.transform.name == "Pyramid" && object2.transform.name == "MagicBox" && object3.transform.name == "Scarab")
+            {
+                if (!isCoffinOpen)
+                {
+                    coffinAnimator.SetTrigger("OpenCoffin");
+                    coffinAudio.Play();
+                    isCoffinOpen = true;
+                }
+            }
+        }
+    }
+
+}
